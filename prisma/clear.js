d560eaf9cd27d9af751308fd9de960faae71f501
SenaNilo <danilo.sena005@gmail.com>
2025-05-06 13:24:37 -0300

    Ajustes

--- prisma/clear.js
+++ prisma/clear.js
@@ -2,7 +2,7 @@ const prisma = require("../src/models/PrismaService");
 
 async function clearDatabase() {
   try {
-    // Deleta em ordem para respeitar chaves estrangeiras
+    
     await prisma.cartItem.deleteMany({});
     await prisma.product.deleteMany({});
     await prisma.user.deleteMany({});
