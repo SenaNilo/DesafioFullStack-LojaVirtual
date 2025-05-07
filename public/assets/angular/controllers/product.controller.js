e2956163721ffc81bd923abcb10ede77d82393e3
SenaNilo <danilo.sena005@gmail.com>
2025-05-07 10:46:41 -0300

    Middleware TOken

---
+++ public/assets/angular/controllers/product.controller.js
@@ -0,0 +1,5 @@
+// @ts-nocheck
+
+window.app.controller('ProductController', function ($scope, $http) {
+
+})
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router(); 
 const AuthController = require('../controller/AuthController'); 
 
+const verificarToken = require('../middlewares/auth.middleware')
+
 router.get('/', AuthController.getUsers)
 // Init no seed
 // router.get('/init', AuthController.initDatabase);
 router.post('/login', AuthController.login); 
 router.delete('/delete/:id', AuthController.deleteUser)
+
+// Middleware
+router.get('/token', verificarToken, async(req, res) => {
+    const userId = req.user.userId
+
+    res.json(userId)
+})
  
 module.exports = router;
