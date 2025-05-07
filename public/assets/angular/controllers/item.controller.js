8406b9267528875a7a6b5285f6bb5d8dd8e05089
SenaNilo <danilo.sena005@gmail.com>
2025-05-07 20:59:57 -0300

    Finzalizado!

--- public/assets/angular/controllers/item.controller.js
+++ public/assets/angular/controllers/item.controller.js
@@ -20,37 +20,7 @@ window.app.controller('ItemController', function ($scope, AuthService, ProductsS
         $scope.item = ProductsService.getProduct();
     }
 
-    $scope.createProduct = async () => {
-        // fazer dps
-        let name = $scope.item.name
-        let price = $scope.item.price
-        let description = $scope.item.description
-
-        try {
-            const response = await fetch('http://localhost:3000/products/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    name,
-                    price,
-                    description
-                })
-            })
-
-            alert("Produto Cadastrado!")
-            $scope.item.name = ""
-            $scope.item.price = 0
-            $scope.item.description = ""
-
-            $scope.$apply()
-            window.location.href = "./cardapio.html";
-        } catch (error) {
-            alert("Produto não Cadastrado!")
-            console.log(error)
-        }
-    }
+    
 
     loadItem();
 });
--- public/assets/angular/controllers/products.controller.js
+++ public/assets/angular/controllers/products.controller.js
@@ -4,6 +4,12 @@ window.app.controller('ProductsController', function ($scope, ProductsService, A
     $scope.products = [];
     $scope.localCurrency = 'R$';
     $scope.search = '';
+    $scope.item = {
+        id: 0,
+        name: '',
+        price: 0,
+        description: ''
+    }
 
     async function loadProducts() {
         $scope.products = await ProductsService.getAll();
@@ -14,6 +20,38 @@ window.app.controller('ProductsController', function ($scope, ProductsService, A
         ProductsService.setProduct(product);
         const prod = ProductsService.getProduct();
         window.location.href = "./product.html";
+    }
+
+    $scope.createProduct = async () => {
+        // fazer dps
+        let name = $scope.item.name
+        let price = $scope.item.price
+        let description = $scope.item.description
+
+        try {
+            const response = await fetch('http://localhost:3000/products/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name,
+                    price,
+                    description
+                })
+            })
+
+            alert("Produto Cadastrado!")
+            $scope.item.name = ""
+            $scope.item.price = 0
+            $scope.item.description = ""
+
+            $scope.$apply()
+            window.location.href = "./cardapio.html";
+        } catch (error) {
+            alert("Produto não Cadastrado!")
+            console.log(error)
+        }
     }
 
     loadProducts();
