1cc333c7d888817f720215abe7113a99bf42fe07
GEdO23 <rm99632@fiap.com.br>
2025-05-08 10:20:23 -0300

    feat: organizar estilização das paginas com sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-    return src('./public/assets/styles/style.scss')
+    return src('./public/assets/styles/**/*.scss')
         .pipe(sass())
         .pipe(dest('./public/assets/styles/'));
 }
 
 function watchTask() {
-    watch(['./public/assets/styles/style.scss'], buildStyles);
+    watch(['./public/assets/styles/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
