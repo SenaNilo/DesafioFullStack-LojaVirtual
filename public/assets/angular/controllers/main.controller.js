9e4d0cd7f328427e973466e998e3c4f699bd7725
GEdO23 <rm99632@fiap.com.br>
2025-05-08 11:35:30 -0300

    perf: converter imagens para formato webp

--- public/assets/angular/controllers/main.controller.js
+++ public/assets/angular/controllers/main.controller.js
@@ -5,31 +5,31 @@ window.app.controller('MainController', function ($scope) {
         {
             name: "Node.js",
             description: "Ambiente de execução JavaScript.",
-            cover: "./assets/img/node-cover.png",
+            cover: "./assets/img/node-cover.webp",
             redirect: "https://nodejs.org/pt"
         },
         {
             name: "SQLite",
             description: "Biblioteca que implementa um mecanismo de banco de dados SQL.",
-            cover: "./assets/img/sqlite-cover.png",
+            cover: "./assets/img/sqlite-cover.webp",
             redirect: "https://www.sqlite.org/"
         },
         {
             name: "Prisma ORM",
             description: "Ferramenta de mapeamento objeto-relacional para Node.js e TypeScript.",
-            cover: "./assets/img/prisma-cover.png",
+            cover: "./assets/img/prisma-cover.webp",
             redirect: "https://www.prisma.io/"
         },
         {
             name: "AngularJS",
             description: "Superheroic JavaScript MVW Framework.",
-            cover: "./assets/img/angular-cover.png",
+            cover: "./assets/img/angular-cover.webp",
             redirect: "https://angularjs.org/"
         },
         {
             name: "Sass",
             description: "CSS com super poderes",
-            cover: "./assets/img/sass-cover.png",
+            cover: "./assets/img/sass-cover.webp",
             redirect: "https://sass-lang.com/"
         }
     ]
